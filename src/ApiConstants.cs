7294dbc0d9b0add8afd646696a9ba8b1a9f2f57c
Jake Soenneker <jake@soenneker.com>
2025-08-16 19:24:34 -0500

    Initial commit

---
+++ src/ApiConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Soenneker.Constants.Apis;
+
+/// <summary>
+/// Common constants typically used in .NET APIs
+/// </summary>
+public static class ApiConstants
+{
+    public const string ControllerHitFlag = nameof(ControllerHitFlag);
+}
---
+++ test/Soenneker.Constants.Apis.Tests/ApiConstantsTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Constants.Apis.Tests;
+
+public sealed class ApiConstantsTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
